ab83f0b7f8530e9021b90d3aaa6b74649b25e07b
Fikri Haikal <fikrihaikal571@gmail.com>
2023-05-22 23:48:41 +0700

    update img Slider - Haikal

--- app/src/main/java/com/axellinoanggoro/binar_e_commerce/view/ui/HomeActivity.kt
+++ app/src/main/java/com/axellinoanggoro/binar_e_commerce/view/ui/HomeActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.axellinoanggoro.binar_e_commerce.R
 import com.axellinoanggoro.binar_e_commerce.databinding.ActivityHomeBinding
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,38 +14,44 @@ import dagger.hilt.android.AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityHomeBinding
+    val imageList = ArrayList<SlideModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bottomNav.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+//        binding.bottomNav.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        imageList.add(SlideModel(R.drawable.test1))
+        imageList.add(SlideModel(R.drawable.test2))
+        imageList.add(SlideModel(R.drawable.test3))
+        binding.imgSlider.setImageList(imageList, ScaleTypes.FIT)
+
 
     }
 
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.home -> {
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.favorite -> {
-                val intent = Intent(this, FavoriteActivity::class.java)
-                startActivity(intent)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.cart -> {
-                val intent = Intent(this, CartActivity::class.java)
-                startActivity(intent)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.account ->{
-                val intent = Intent(this,ProfileActivity::class.java)
-                startActivity(intent)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+//    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//        when (item.itemId) {
+//            R.id.home -> {
+//                val intent = Intent(this, HomeActivity::class.java)
+//                startActivity(intent)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.favorite -> {
+//                val intent = Intent(this, FavoriteActivity::class.java)
+//                startActivity(intent)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.cart -> {
+//                val intent = Intent(this, CartActivity::class.java)
+//                startActivity(intent)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.account ->{
+//                val intent = Intent(this,ProfileActivity::class.java)
+//                startActivity(intent)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//        }
+//        false
+//    }
 }
--- app/src/main/java/com/axellinoanggoro/binar_e_commerce/view/ui/ProfileActivity.kt
+++ app/src/main/java/com/axellinoanggoro/binar_e_commerce/view/ui/ProfileActivity.kt
@@ -1,14 +1,48 @@
 package com.axellinoanggoro.binar_e_commerce.view.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.axellinoanggoro.binar_e_commerce.R
+import com.axellinoanggoro.binar_e_commerce.databinding.ActivityProfileBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ProfileActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        binding.bottomNav.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
     }
+//    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//        when (item.itemId) {
+//            R.id.home -> {
+//                val intent = Intent(this, HomeActivity::class.java)
+//                startActivity(intent)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.favorite -> {
+//                val intent = Intent(this, FavoriteActivity::class.java)
+//                startActivity(intent)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.cart -> {
+//                val intent = Intent(this, CartActivity::class.java)
+//                startActivity(intent)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.account ->{
+//                val intent = Intent(this,ProfileActivity::class.java)
+//                startActivity(intent)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//        }
+//        false
+//    }
 }
