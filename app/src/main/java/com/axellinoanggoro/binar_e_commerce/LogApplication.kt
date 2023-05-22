5601e4f27509d49e3a2dc435ccd6e871d126cae7
Fikri Haikal <fikrihaikal571@gmail.com>
2023-05-22 21:26:21 +0700

    update splash, mainactvity, google

---
+++ app/src/main/java/com/axellinoanggoro/binar_e_commerce/LogApplication.kt
@@ -0,0 +1,9 @@
+package com.axellinoanggoro.binar_e_commerce
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class LogApplication : Application() {
+}
--- app/src/main/java/com/axellinoanggoro/binar_e_commerce/MainActivity.kt
+++ app/src/main/java/com/axellinoanggoro/binar_e_commerce/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.axellinoanggoro.binar_e_commerce
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.axellinoanggoro.binar_e_commerce.databinding.ActivityMainBinding
+import com.axellinoanggoro.binar_e_commerce.view.ui.HomeActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity,HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
