340ee6ec1386c09b16a5f07185429e3d269a2fc9
Fikri Haikal <fikrihaikal571@gmail.com>
2023-05-22 23:03:56 +0700

    update-haikal

---
+++ app/src/main/java/com/axellinoanggoro/binar_e_commerce/view/ui/CartActivity.kt
@@ -0,0 +1,14 @@
+package com.axellinoanggoro.binar_e_commerce.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.axellinoanggoro.binar_e_commerce.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
