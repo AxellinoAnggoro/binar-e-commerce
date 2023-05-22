5a999ebbbe61fcfe8f5c6f747a71c5798acc07a5
Fikri Haikal <fikrihaikal571@gmail.com>
2023-05-22 20:13:10 +0700

    update act dan gradle crashlytic, connect firebase auth

---
+++ app/src/main/java/com/axellinoanggoro/binar_e_commerce/view/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.axellinoanggoro.binar_e_commerce.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.axellinoanggoro.binar_e_commerce.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/axellinoanggoro/binar_e_commerce/view/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.axellinoanggoro.binar_e_commerce.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.axellinoanggoro.binar_e_commerce.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
