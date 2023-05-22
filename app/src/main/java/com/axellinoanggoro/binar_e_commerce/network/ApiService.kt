652575576da6e736e3dfa882ca6ee02517fd0cb8
AxellinoAnggoro <axellinoanggoro1311@gmail.com>
2023-05-22 22:29:37 +0700

    add api method and item news

--- app/src/main/java/com/axellinoanggoro/binar_e_commerce/network/ApiService.kt
+++ app/src/main/java/com/axellinoanggoro/binar_e_commerce/network/ApiService.kt
@@ -1,4 +1,24 @@
 package com.axellinoanggoro.binar_e_commerce.network
 
+import com.axellinoanggoro.binar_e_commerce.model.GetNewsUpdateItem
+import com.axellinoanggoro.binar_e_commerce.model.GetSlidersItem
+import com.axellinoanggoro.binar_e_commerce.model.GetUsersItem
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ApiService {
+    @GET("news")
+    fun getAllNews(): Call<List<GetNewsUpdateItem>>
+
+    @GET("users")
+    fun getAllUsers(): Call<List<GetUsersItem>>
+
+    @GET("sliders/{id}")
+    fun getSliderById(
+        @Path("id") id : Int,
+    ):Call<List<GetSlidersItem>>
+
+
+
 }
