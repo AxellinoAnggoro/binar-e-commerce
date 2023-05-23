c3bae05c042b11f65b50a0c7829b580f62fd5812
Fikri Haikal <fikrihaikal571@gmail.com>
2023-05-24 00:45:35 +0700

    update ui detail rv home - Fikri Haikal

--- app/src/main/java/com/axellinoanggoro/binar_e_commerce/network/ApiService.kt
+++ app/src/main/java/com/axellinoanggoro/binar_e_commerce/network/ApiService.kt
@@ -21,4 +21,5 @@ interface ApiService {
 
 
 
+
 }
--- app/src/main/java/com/axellinoanggoro/binar_e_commerce/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/axellinoanggoro/binar_e_commerce/viewmodel/HomeViewModel.kt
@@ -1,4 +1,16 @@
 package com.axellinoanggoro.binar_e_commerce.viewmodel
 
-class HomeViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.axellinoanggoro.binar_e_commerce.model.GetSlidersItem
+import com.axellinoanggoro.binar_e_commerce.network.ApiService
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(private val api:ApiService): ViewModel() {
+    var liveDataProduct : MutableLiveData<List<GetSlidersItem>> = MutableLiveData()
+
+
 }
